fc39c1664b7a4a51b6ea0ee24454d3fa2a487fd6
Fritz-02 <fritz4425@gmail.com>
2022-01-29 10:03:18 -0800

    Changed version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dgg-bot",
-    version="0.1.0",
+    version="0.1.1",
     author="Fritz",
     description="A library for making a bot in Destiny.gg chat.",
     long_description_content_type="text/markdown",
