329d26709efa35f0f9c3459126bc9b1362e8f404
Fritz-02 <fritz4425@gmail.com>
2022-02-03 10:29:30 -0800

    Added MuteMessage, made PrivateMessage subclass of Message, moved message logging into the Message class, and added a reply function.

--- dggbot/message.py
+++ dggbot/message.py
@@ -1,22 +1,31 @@
 import dataclasses
+import logging
 
 
 @dataclasses.dataclass
 class Message:
+    chat: "DGGChat"
     type: str
     nick: str = None
     features: list = None
     timestamp: int = None
     data: str = None
 
+    def __post_init__(self):
+        logging.debug(self)
+
+    def reply(self, content):
+        self.chat.send(content)
+
 
 @dataclasses.dataclass
-class PrivateMessage:
-    type: str
-    nick: str = None
-    timestamp: int = None
-    data: str = None
+class PrivateMessage(Message):
     message_id: str = None
 
+    def reply(self, content):
+        self.chat.send_privmsg(self.nick, content)
 
 
+@dataclasses.dataclass
+class MuteMessage(Message):
+    duration: int = None
