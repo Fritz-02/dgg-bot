c1dd9473e3816ea77bf54d7c12c52172f019e4a2
Fritz-02 <fritz4425@gmail.com>
2022-02-03 11:14:35 -0800

    Updated version and classifiers.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dgg-bot",
-    version="0.1.2",
+    version="0.1.3",
     author="Fritz",
     description="A library for making a bot in Destiny.gg chat.",
     long_description_content_type="text/markdown",
@@ -12,9 +12,14 @@ setup(
         "Bug Tracker": "https://github.com/Fritz-02/dgg-bot/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta"
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Communications :: Chat"
     ],
     packages=find_packages(),
     python_requires=">=3.7",
