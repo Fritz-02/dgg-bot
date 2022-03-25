9b00cc78acfc8a1304fa9f252a67c8d4ee4b0d8e
Fritz-02 <fritz4425@gmail.com>
2022-03-25 08:43:43 -0700

    Added more errors.

--- dggbot/errors.py
+++ dggbot/errors.py
@@ -1,6 +1,43 @@
+
+class AccountTooYoung(Exception):
+    pass
+
+
 class Banned(Exception):
     pass
 
 
+class DuplicateMessage(Exception):
+    pass
+
+
+class InvalidMessage(Exception):
+    pass
+
+
+class NeedLogin(Exception):
+    pass
+
+
+class NoPermission(Exception):
+    pass
+
+
+class NotFound(Exception):
+    pass
+
+
+class ProtocolError(Exception):
+    pass
+
+
+class SubMode(Exception):
+    pass
+
+
 class Throttled(Exception):
+    pass
+
+
+class TooManyConnections(Exception):
     pass
