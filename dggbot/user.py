b07307537cc38be764782f55b222c6bb7a0e4d7c
Fritz <fritz4425@gmail.com>
2023-01-11 07:44:40 -0800

    Added user created date property.

--- dggbot/user.py
+++ dggbot/user.py
@@ -1,7 +1,9 @@
 import dataclasses
+from datetime import datetime
 
 
 @dataclasses.dataclass
 class User:
     name: str
+    created_date: datetime
     features: list = dataclasses.field(default_factory=list)
