0fe43e1567c757f13a4eb4aeee991ed6dc8727a1
Fritz <fritz4425@gmail.com>
2023-03-12 19:12:53 -0700

    Added Flair class

---
+++ dggbot/flairs.py
@@ -0,0 +1,23 @@
+import dataclasses
+import requests
+
+
+@dataclasses.dataclass
+class Flair:
+    label: str
+    name: str
+    hidden: bool
+    priority: int
+    color: str
+    rainbowColor: bool
+    image: list[dict]
+
+    def __repr__(self):
+        return f"Flair<{self.label}>"
+
+
+def flair_converter(endpoint: str) -> dict:
+    """Returns a dict to convert flair names (e.g. flair17) to a Flair object."""
+    r = requests.get(endpoint)
+    converter = {item["name"]: Flair(**item) for item in r.json()}
+    return converter
