3cdcdf71d3ab98aa14f98994fbe6e76ebbf8e6d5
Fritz <fritz4425@gmail.com>
2023-04-27 18:18:03 -0700

    Updated example

--- examples/live.py
+++ examples/live.py
@@ -20,20 +20,21 @@ def sec_to_hm(sec: int) -> str:
 
 @live.event()
 def on_streaminfo(streaminfo: StreamInfo):
-    if streaminfo.live:
-        started_at = datetime.strptime(streaminfo.started_at, "%Y-%m-%dT%H:%M:%S+0000")
+    if streaminfo.is_live():
+        stream = streaminfo.get_livestreams()[0]
+        started_at = datetime.strptime(stream.started_at, "%Y-%m-%dT%H:%M:%S+0000")
         started_ago = datetime.utcnow() - started_at
         print(
             f"Viewers: {streaminfo.viewers}. Stream live as of {sec_to_hm(started_ago.seconds)} ago"
         )
     else:
-        ended_at = datetime.strptime(streaminfo.ended_at, "%Y-%m-%dT%H:%M:%S+0000")
+        ended_at = datetime.strptime(streaminfo.youtube.ended_at, "%Y-%m-%dT%H:%M:%S+0000")
         last_online = datetime.utcnow() - ended_at
         s = ["Stream was last online"]
         if last_online.days:
             s.append(f"{last_online.days}d")
         s.append(
-            f"{sec_to_hm(last_online.seconds)} ago. Time Streamed: {sec_to_hm(streaminfo.duration)}"
+            f"{sec_to_hm(last_online.seconds)} ago. Time Streamed: {sec_to_hm(streaminfo.youtube.duration)}"
         )
         print(" ".join(s))
 
@@ -44,20 +45,22 @@ def on_stream_start():
 
 
 @live.event()
-def on_youtubevideos(videos: tuple[YoutubeVideo]):
-    print("Videos:")
+def on_videos(videos: tuple[YoutubeVideo]):
+    s = ["Videos:"]
     for video in videos:
-        print(f"\t{video.title} ({video.url})")
+        s.append(f"\t{video.title} ({video.url})")
+    print("\n".join(s))
 
 
 @live.event()
-def on_youtubevods(vods: tuple[YoutubeVideo]):
-    print("Vods:")
+def on_youtubevods(vods: tuple):
+    s = ["Vods:"]
     for vod in vods:
         start = datetime.strptime(vod.streamStartTime, "%Y-%m-%dT%H:%M:%S+0000")
         end = datetime.strptime(vod.streamEndTime, "%Y-%m-%dT%H:%M:%S+0000")
         duration = end - start
-        print(f"\t{vod.title} ({vod.url}). Streamed for {sec_to_hm(duration.seconds)}")
+        s.append(f"\t{vod.title} ({vod.url}). Streamed for {sec_to_hm(duration.seconds)}")
+    print("\n".join(s))
 
 
 if __name__ == "__main__":
