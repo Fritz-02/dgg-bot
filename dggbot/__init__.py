a58d0d00f73f9a783a084a3685a0b43e93b292ca
Fritz <fritz4425@gmail.com>
2023-05-26 07:15:08 -0700

    Version bump, added more imports

--- dggbot/__init__.py
+++ dggbot/__init__.py
@@ -1,7 +1,9 @@
 from .bot import DGGBot
 from .chat import DGGChat
 from .event import EventType
-from .live import DGGLive, StreamInfo, YoutubeVideo
-from .message import Message, PrivateMessage, MuteMessage, PinnedMessage
+from .flairs import Flair
+from .live import DGGLive
+from .live.message import Stream, StreamInfo, YoutubeVideo, YoutubeVod
+from .message import Message, MuteMessage, PrivateMessage, PinnedMessage, PollMessage, VoteMessage
 
-VERSION = "0.15.0"
+VERSION = "1.0.0"
