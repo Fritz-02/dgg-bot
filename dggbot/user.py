ca2bc864ea1c93bb88266049c38a349bca9d69ee
Fritz <fritz4425@gmail.com>
2023-05-27 18:55:49 -0700

    Added mention functions to User

--- dggbot/user.py
+++ dggbot/user.py
@@ -1,5 +1,12 @@
+from __future__ import annotations
 import dataclasses
 from datetime import datetime
+from functools import cache
+import re
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .message import Message
 
 
 @dataclasses.dataclass
@@ -8,3 +15,13 @@ class User:
     name: str
     created_date: datetime
     features: list = dataclasses.field(default_factory=list)
+
+    @cache
+    def _name_regex(self) -> re.Pattern:
+        return re.compile(rf"\b{self.name}\b", re.IGNORECASE)
+
+    def is_mentioned(self, msg: Message) -> bool:
+        return bool(self._name_regex().search(msg.data))
+
+    def __hash__(self):
+        return hash((self.id, self.name, self.created_date, tuple(self.features)))
